73f74ebaa41a6c5a445380d3215aa76bd71edb36
Piotr Kozimor <p1996k@gmail.com>
2020-05-30 11:58:41 +0200

    refactor: move envoy configuration file

--- build/envoy.sh
+++ build/envoy.sh
@@ -1,5 +1,5 @@
 cont=$(buildah from envoyproxy/envoy:v1.14.1)
-buildah copy $cont envoy.yaml /etc/envoy/envoy.yaml
+buildah copy $cont build/envoy.yaml /etc/envoy/envoy.yaml
 buildah copy $cont fullchain.pem /etc/pki/fullchain.pem
 buildah copy $cont privkey.pem /etc/pki/privkey.pem
 buildah commit --format docker $cont krkstops-envoy
