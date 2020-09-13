2590f7467f8cc254b7dca3158fc3c6f08dbd5023
Piotr Kozimor <p1996k@gmail.com>
2020-09-13 22:33:06 +0200

    refactor: use Cobra in krkstopsctl

--- cmd/krkstops/krkstopsctl.go
+++ cmd/krkstops/krkstopsctl.go
@@ -2,21 +2,97 @@ package main
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"io"
 	"log"
-	"os"
-	"text/tabwriter"
 	"time"
 
 	"github.com/PiotrKozimor/krk-stops-backend-golang/krkstops"
 	pb "github.com/PiotrKozimor/krk-stops-backend-golang/krkstops-grpc"
+	"github.com/spf13/cobra"
 	grpc "google.golang.org/grpc"
 )
 
-var conn grpc.ClientConn
-var client pb.KrkStopsClient
+var (
+	rootCmd = &cobra.Command{
+		Use:  "krkstopsctl",
+		Long: `krkstopsctl interacts with krk-stops.pl backend`,
+	}
+	airlyCmd = &cobra.Command{
+		Use:   "airly",
+		Short: "Query air quality, temperature and humidity from airly installation",
+		Run: func(cmd *cobra.Command, args []string) {
+			airly, err := client.GetAirly(ctx, &pb.Installation{Id: airlyId})
+			if err != nil {
+				log.Fatalf("fail to get airly: %v", err)
+			}
+			krkstops.PrintAirly(airly)
+		},
+	}
+	depsCmd = &cobra.Command{
+		Use:   "deps",
+		Short: "Query departures from given stop",
+		Run: func(cmd *cobra.Command, args []string) {
+			depsStrem, err := client.GetDepartures(ctx, &pb.Stop{ShortName: fmt.Sprint(stopId)})
+			if err != nil {
+				log.Fatalf("fail to get departures: %v", err)
+			}
+			for {
+				departure, err := depsStrem.Recv()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Println(departure)
+			}
+		},
+	}
+	stopsCmd = &cobra.Command{
+		Use:   "stops",
+		Short: "Search stops",
+		Run: func(cmd *cobra.Command, args []string) {
+			stopsStream, err := client.SearchStops(ctx, &pb.StopSearch{Query: args[0]})
+			if err != nil {
+				log.Fatalf("fail to search stops: %v", err)
+			}
+			for {
+				stop, err := stopsStream.Recv()
+				if err == io.EOF {
+					break
+				}
+				if err != nil {
+					log.Fatal(err)
+				}
+				log.Println(stop)
+			}
+		},
+		Args: cobra.ExactArgs(1),
+	}
+
+	airlyId int32
+	stopId  int32
+	query   string
+	conn    grpc.ClientConn
+	client  pb.KrkStopsClient
+	ctx     context.Context
+)
+
+func init() {
+	endpoint := rootCmd.Flags().StringP("endpoint", "e", "krk-stops.pl:8080", "backend url address")
+	depsCmd.Flags().Int32Var(&stopId, "id", 610, "stop id, find it by using stops command")
+	airlyCmd.Flags().Int32Var(&airlyId, "id", 9914, "installation id")
+	rootCmd.AddCommand(depsCmd)
+	rootCmd.AddCommand(airlyCmd)
+	rootCmd.AddCommand(stopsCmd)
+	conn, err := grpc.Dial(*endpoint, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("fail to dial: %v", err)
+	}
+	client = pb.NewKrkStopsClient(conn)
+	ctx, _ = context.WithTimeout(context.Background(), 1*time.Second)
+}
 
 func krkstopsUsage() {
 	fmt.Printf(`Query KrkStops API
@@ -27,68 +103,5 @@ func krkstopsUsage() {
 }
 
 func main() {
-	w := tabwriter.NewWriter(os.Stdout, 1, 2, 2, ' ', 0)
-	var endpoint string = "krk-stops.pl"
-	depsCmd := flag.NewFlagSet("deps", flag.ExitOnError)
-	// stopsCmd := flag.NewFlagSet("stops", flag.ExitOnError)
-	airlyCmd := flag.NewFlagSet("airly", flag.ExitOnError)
-	var airlyID = airlyCmd.Int("id", 9914, "ID of Airly installation to query")
-	var depShortName = depsCmd.String("id", "610", "shortName of stop to query")
-	flag.Usage = krkstopsUsage
-	flag.Parse()
-	endpointEnv := os.Getenv("ENDPOINT")
-	if endpointEnv != "" {
-		endpoint = endpointEnv
-	}
-	fmt.Println(endpoint)
-	conn, err := grpc.Dial(fmt.Sprintf("%s:8080", endpoint), grpc.WithInsecure())
-	defer conn.Close()
-	if err != nil {
-		log.Fatalf("fail to dial: %v", err)
-	}
-	client := pb.NewKrkStopsClient(conn)
-	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
-	defer cancel()
-	switch os.Args[1] {
-	case "airly":
-		airlyCmd.Parse(os.Args[2:])
-		airly, err := client.GetAirly(ctx, &pb.Installation{Id: int32(*airlyID)})
-		if err != nil {
-			log.Fatalf("fail to get airly: %v", err)
-		}
-		krkstops.PrintAirly(w, airly)
-	case "deps":
-		depsCmd.Parse(os.Args[2:])
-		depsStrem, err := client.GetDepartures(ctx, &pb.Stop{ShortName: *depShortName})
-		if err != nil {
-			log.Fatalf("fail to get departures: %v", err)
-		}
-		for {
-			departure, err := depsStrem.Recv()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Println(departure)
-		}
-	case "stops":
-		stopsStream, err := client.SearchStops(ctx, &pb.StopSearch{Query: os.Args[2]})
-		if err != nil {
-			log.Fatalf("fail to search stops: %v", err)
-		}
-		for {
-			stop, err := stopsStream.Recv()
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				log.Fatal(err)
-			}
-			log.Println(stop)
-		}
-	default:
-		fmt.Print("Please provide correct command!")
-	}
+	rootCmd.Execute()
 }
