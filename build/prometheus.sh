2dfdeb69d89ae58244e337ef2d6c323373eb9733
Piotr Kozimor <p1996k@gmail.com>
2020-10-04 13:22:16 +0200

    feat: setup and build prometheus image

---
+++ build/prometheus.sh
@@ -0,0 +1,7 @@
+set -xe
+cont=$(buildah from prom/prometheus)
+buildah copy $cont ./prometheus.yaml /etc/prometheus/
+buildah commit --format docker $cont krkstops-prometheus
+buildah tag krkstops-prometheus docker.io/narciarz96/krkstops-prometheus:latest
+buildah push docker.io/narciarz96/krkstops-prometheus:latest
+buildah rm $cont
