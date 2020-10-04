776a9dc71d03ea0dc34259347d54529e3021cbed
Piotr Kozimor <p1996k@gmail.com>
2020-10-04 13:54:57 +0200

    fix: parse endpoint flag correctly in krkstopsctl

--- cmd/krkstops/krkstopsctl.go
+++ cmd/krkstops/krkstopsctl.go
@@ -22,6 +22,7 @@ var (
 		Use:   "airly",
 		Short: "Query air quality, temperature and humidity from airly installation",
 		Run: func(cmd *cobra.Command, args []string) {
+			initClient()
 			airly, err := client.GetAirly(ctx, &pb.Installation{Id: airlyId})
 			if err != nil {
 				log.Fatalf("fail to get airly: %v", err)
@@ -33,6 +34,7 @@ var (
 		Use:   "deps",
 		Short: "Query departures from given stop",
 		Run: func(cmd *cobra.Command, args []string) {
+			initClient()
 			depsStrem, err := client.GetDepartures(ctx, &pb.Stop{ShortName: fmt.Sprint(stopId)})
 			if err != nil {
 				log.Fatalf("fail to get departures: %v", err)
@@ -53,6 +55,7 @@ var (
 		Use:   "stops",
 		Short: "Search stops",
 		Run: func(cmd *cobra.Command, args []string) {
+			initClient()
 			stopsStream, err := client.SearchStops(ctx, &pb.StopSearch{Query: args[0]})
 			if err != nil {
 				log.Fatalf("fail to search stops: %v", err)
@@ -71,27 +74,31 @@ var (
 		Args: cobra.ExactArgs(1),
 	}
 
-	airlyId int32
-	stopId  int32
-	query   string
-	conn    grpc.ClientConn
-	client  pb.KrkStopsClient
-	ctx     context.Context
+	airlyId  int32
+	stopId   int32
+	query    string
+	conn     grpc.ClientConn
+	client   pb.KrkStopsClient
+	ctx      context.Context
+	endpoint string
 )
 
-func init() {
-	endpoint := rootCmd.Flags().StringP("endpoint", "e", "krk-stops.pl:8080", "backend url address")
-	depsCmd.Flags().Int32Var(&stopId, "id", 610, "stop id, find it by using stops command")
-	airlyCmd.Flags().Int32Var(&airlyId, "id", 9914, "installation id")
-	rootCmd.AddCommand(depsCmd)
-	rootCmd.AddCommand(airlyCmd)
-	rootCmd.AddCommand(stopsCmd)
-	conn, err := grpc.Dial(*endpoint, grpc.WithInsecure())
+func initClient() {
+	conn, err := grpc.Dial(endpoint, grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("fail to dial: %v", err)
 	}
 	client = pb.NewKrkStopsClient(conn)
 	ctx, _ = context.WithTimeout(context.Background(), 1*time.Second)
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringVarP(&endpoint, "endpoint", "e", "krk-stops.pl:8080", "backend url address")
+	depsCmd.Flags().Int32Var(&stopId, "id", 610, "stop id, find it by using stops command")
+	airlyCmd.Flags().Int32Var(&airlyId, "id", 9914, "installation id")
+	rootCmd.AddCommand(depsCmd)
+	rootCmd.AddCommand(airlyCmd)
+	rootCmd.AddCommand(stopsCmd)
 }
 
 func krkstopsUsage() {
