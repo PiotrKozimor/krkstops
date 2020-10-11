e786487659f2c92c23dece845d61c5c0918a625c
Piotr Kozimor <p1996k@gmail.com>
2020-10-11 20:14:56 +0200

    fix: add endpoint in stops update command

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,4 +3,4 @@ rsync cmd/* azure:krkstops
 scp build/run_backend.sh azure:krkstops
 scp KRKSTOPS azure:krkstops
 ssh azure sudo bash /home/piotr/krkstops/run_backend.sh
-ssh azure /home/piotr/krkstops/stopsctl update -i >> /dev/null
+ssh azure /home/piotr/krkstops/stopsctl update -e 10.88.0.7:6379 -i
