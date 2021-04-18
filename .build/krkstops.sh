2d4ba430976c66cd4831762c2afd02f1b36c6309
Piotr Kozimor <p1996k@gmail.com>
2021-04-18 13:36:36 +0200

    ci: build tag if provided and latest, otherwise latest only

--- .build/krkstops.sh
+++ .build/krkstops.sh
@@ -3,6 +3,15 @@ cont=$(buildah from gcr.io/distroless/base-debian10)
 buildah copy $cont cmd/krkstops/krkstops /bin
 buildah config --entrypoint '["/bin/krkstops"]' --port 8080 $cont
 buildah commit --format docker $cont krkstops
-buildah tag krkstops docker.io/narciarz96/krkstops:$1
-buildah push docker.io/narciarz96/krkstops:$1
+if [ -z $1 ] 
+then
+    :
+else
+    echo "👌 Building $1"
+    buildah tag krkstops docker.io/narciarz96/krkstops:$1
+    buildah push docker.io/narciarz96/krkstops:$1
+fi
+echo "🌝 Building only latest"
+buildah tag krkstops docker.io/narciarz96/krkstops:latest
+buildah push docker.io/narciarz96/krkstops:latest
 buildah rm $cont
--- .ci/build.sh
+++ .ci/build.sh
@@ -1,8 +1,3 @@
 TAG=$1
-if [ -z $1 ] 
-then
-    TAG=latest
-fi
-echo $TAG
 bash .ci/build_ctl.sh
 .build/krkstops.sh $TAG
