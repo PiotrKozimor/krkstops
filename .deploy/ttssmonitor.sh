fd640d9c9820421d34a4e416c2d88a16f21d08f6
Piotr Kozimor <p1996k@gmail.com>
2021-07-28 20:32:03 +0200

    fix: ttssmonitor deploy script

--- .deploy/ttssmonitor.sh
+++ .deploy/ttssmonitor.sh
@@ -1,4 +1,4 @@
-running_prom=$(podman container ls | grep krkstops-prometheus: | awk '{print $1}')
+running_prom=$(podman container ls | grep prometheus: | awk '{print $1}')
 running_monitor=$(podman container ls | grep krkstops-ttssmonitor: | awk '{print $1}')
 prometheus=$(podman create --privileged --ip 10.88.0.8 -v /home/piotr/prom:/prometheus -v /home/piotr/krkstops/prometheus.yml:/etc/prometheus/prometheus.yml docker.io/prom/prometheus:v2.28.1)
 ttssmonitor=$(podman create --ip 10.88.0.10 docker.io/narciarz96/krkstops-ttssmonitor:$1)
