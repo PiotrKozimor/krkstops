6bb58fd0c56e58eba098b8f052f003b4595d87b6
Piotr Kozimor <p1996k@gmail.com>
2021-11-28 13:11:12 +0100

    Build ttssmonitor and krkstops from scratch

--- .build/krkstops.sh
+++ .build/krkstops.sh
@@ -1,11 +1,11 @@
 set -ex
 cd cmd/krkstops
-go build 
+CGO_ENABLED=0 go build .
 cd ../..
-cont=$(buildah from gcr.io/distroless/base-debian10)
-buildah copy $cont cmd/krkstops/krkstops /bin
+cont=$(buildah from scratch)
+buildah copy $cont cmd/krkstops/krkstops /bin/krkstops
 buildah config --entrypoint '["/bin/krkstops"]' --port 8080 --port 9090 $cont
-buildah commit --format docker $cont krkstops
+buildah commit $cont krkstops
 echo "👌 Tag nad push $1"
 buildah tag krkstops docker.io/narciarz96/krkstops:$1
 buildah push docker.io/narciarz96/krkstops:$1
--- .build/ttssmonitor.sh
+++ .build/ttssmonitor.sh
@@ -1,12 +1,12 @@
 set -ex
 cd cmd/ttssmonitord
-go build 
+CGO_ENABLED=0 go build .
 cd ../..
-cont=$(buildah from gcr.io/distroless/base-debian10)
-buildah copy $cont cmd/ttssmonitord /bin
+cont=$(buildah from scratch)
+buildah copy $cont cmd/ttssmonitord/ttssmonitord /bin/ttssmonitord
 buildah config --entrypoint '["/bin/ttssmonitord"]' --port 8080 $cont
 buildah config --env LOGLEVEL=error $cont
-buildah commit --format docker $cont krkstops-ttssmonitor
+buildah commit $cont krkstops-ttssmonitor
 echo "👌 Tag nad push $1"
 buildah tag krkstops-ttssmonitor docker.io/narciarz96/krkstops-ttssmonitor:$1
 buildah push docker.io/narciarz96/krkstops-ttssmonitor:$1
