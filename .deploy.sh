a58b4ca908b5661b5e0af05fa74982607cf2aa91
Piotr Kozimor <p1996k@gmail.com>
2025-06-16 16:55:24 +0200

    Change deployment method

--- .deploy.sh
+++ .deploy.sh
@@ -1,39 +1,7 @@
 #!/bin/bash
 set -e
-
-TAG=$(git tag --points-at HEAD)
-if [ -z $TAG ] 
-then
-    TAG=$(git rev-parse --short HEAD)
-fi
-echo "TAG: $TAG"
-
-help () {
-	echo "Deploy"
-	echo "	-k krkstops"
-}
-
-krkstops () {
-    ssh coreos podman pull docker.io/narciarz96/krkstops:$TAG
-    ssh coreos systemctl stop krkstops.service
-    ssh coreos podman rm -i krkstops
-    ssh coreos podman create \
-            --name krkstops \
-            --network podman \
-            --ip 10.88.0.10 \
-            docker.io/narciarz96/krkstops:$TAG
-    ssh coreos systemctl start krkstops.service
-}
-
-while getopts hk opts; do
-   case ${opts} in
-    h) help; exit 0;;
-    k) krkstops;;
-   esac
-done
-
-
-
-
-set -xe
-
+cd cmd/krkstops
+GOARCH=arm64 CGO_ENABLED=0 go build .
+ssh coreos systemctl stop krkstops.service
+scp krkstops coreos:/var/krkstops/krkstops
+ssh coreos systemctl start krkstops.service
---
+++ .deploy/krkstops.sh
@@ -0,0 +1,8 @@
+podman create \
+  --name krkstops \
+  -v /var/krkstops:/app:z \
+  --workdir /app \
+  --entrypoint /app/krkstops \
+  --network podman \
+  --ip 10.88.0.10 \
+  docker.io/library/busybox:stable-musl
